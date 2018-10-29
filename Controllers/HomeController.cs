f3c4de3f4047ef222fdda71cb48626fa33640fe6
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2018-10-29 13:42:10 +0700

    Add Au Page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace hackathon_pau.Controllers
             return View();
         }
 
+        public IActionResult Au()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
